45704d92a828115e107616b4f1f33e41a2ec10f9
Sanaa <ahdoursanaa@gmail.com>
2023-07-22 14:39:56 +0400

    vfinale

--- FrontEnd/Script/modal.js
+++ FrontEnd/Script/modal.js
@@ -25,6 +25,124 @@ const editBtnDisplay = () => {
 }
 editBtnDisplay()
 
+function swipeModal() {
+  const mod1 = document.querySelector("#modal1");
+  const mod2 = document.querySelector("#modal2");
+  if (mod1.style.display !== "none") {
+    mod1.style.display = "none";
+    mod2.style.display = "flex";
+  
+  } else {
+    mod1.style.display = "flex";
+    mod2.style.display = "none";
+  }
+}
+const addbutton = document.querySelector("#add-validated");
+addbutton.addEventListener("click", swipeModal);
+
+const ret = document.querySelector(".modal-previous");
+ret.addEventListener("click", swipeModal);
+
+
+document.querySelector('.select-image').addEventListener('click', function () {
+  document.getElementById('file').click();
+});
+
+document.getElementById("add-picture-btn").addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const fileInput = document.getElementById("file");
+  const btnTitle = document.querySelector("#add-title");
+  const categoryOfWork = document.querySelector("#add-category");
+
+  // Vérifier si une image a été sélectionnée
+  if (!fileInput.files || fileInput.files.length === 0) {
+    alert("Veuillez sélectionner une image !");
+    return;
+  }
+
+  // Vérifier si champ titre est vide
+  if (btnTitle.value.trim() === "") {
+    alert("Erreur: Le titre doit être renseigné.");
+    return;
+  }
+
+  // Vérifier si champ catégorie est vide
+  if (categoryOfWork.value.trim() === "") {
+    alert("Erreur: La catégorie doit être renseignée.");
+    return;
+  }
+
+  formData.append("title", btnTitle.value);
+  formData.append("category", categoryOfWork.value);
+
+  fetch(worksUrl, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${sessionStorage.getItem("token")}`,
+      'Accept': 'application/json'
+    },
+    body: formData
+  })
+    .then(response => {
+      response.json().then(response2 => {
+        works.push(response2)
+        displayWorksModal()
+        displayWorks()
+        closeModal(e)
+      })
+    })
+    .catch(error => {
+      alert("Une erreur est survenue lors de l'ajout de l'image : " + error.message);
+    });
+});
+
+document.getElementById("file").addEventListener('change', function (e) {
+  e.preventDefault();
+  const file = e.target.files[0];
+  formData = new FormData();
+
+
+  // Vérifier la taille de l'image (maximum de 4 Mo)
+  const maxSize = 4 * 1024 * 1024; // 4 Mo en octets
+  if (file.size > maxSize) {
+    alert("Erreur: la taille de l'image ne doit pas dépasser 4 Mo.");
+    return;
+  }
+
+  // Vérifier si le fichier est au format PNG, JPEG ou PJPEG
+  const validImageFormats = ["image/png", "image/jpeg", "image/pjpeg"];
+  if (!validImageFormats.includes(file.type)) {
+    alert("Erreur: format de l'image non valide.");
+    return;
+  }
+
+  formData.append("image", file);
+
+  const imagePreview = document.getElementById('image-preview');
+  const uploadBtnContainer = document.getElementById('upload-btn-container');
+
+  const reader = new FileReader();
+  reader.onload = function (event) {
+    imagePreview.src = event.target.result;
+    // Rendre la miniature visible
+    imagePreview.style.display = "block";
+
+    // Masquer l'icône et le bouton "Ajouter Image"
+    uploadBtnContainer.style.display = "none";
+  };
+
+  reader.onerror = function () {
+    // Afficher l'icône et le bouton "Ajouter Image" en cas d'erreur
+    imagePreview.style.display = "none";
+    uploadBtnContainer.style.display = "block";
+
+    alert("Erreur lors du chargement de l'image !");
+  };
+
+  reader.readAsDataURL(file);
+});
+
 
 const openModal = function (e) {
   closeModal(e)
@@ -42,110 +160,11 @@ const openModal = function (e) {
   }
   else {
 
-    document.querySelector('.select-image').addEventListener('click', function () {
-      document.getElementById('file').click();
-    });
 
-    document.getElementById("add-picture-btn").addEventListener('click', function (e) {
-      e.preventDefault();
-
-      const fileInput = document.getElementById("file");
-      const btnTitle = document.querySelector("#add-title");
-      const categoryOfWork = document.querySelector("#add-category");
-
-      // Vérifier si une image a été sélectionnée
-      if (!fileInput.files || fileInput.files.length === 0) {
-        alert("Veuillez sélectionner une image !");
-        return;
-      }
-
-      // Vérifier si champ titre est vide
-      if (btnTitle.value.trim() === "") {
-        alert("Erreur: Le titre doit être renseigné.");
-        return;
-      }
-
-      // Vérifier si champ catégorie est vide
-      if (categoryOfWork.value.trim() === "") {
-        alert("Erreur: La catégorie doit être renseignée.");
-        return;
-      }
-
-      formData.append("title", btnTitle.value);
-      formData.append("category", categoryOfWork.value);
-
-      fetch(worksUrl, {
-        method: 'POST',
-        headers: {
-          'Authorization': `Bearer ${sessionStorage.getItem("token")}`,
-          'Accept': 'application/json'
-        },
-        body: formData
-      })
-        .then(response => {
-          response.json().then(response2 => {
-            works.push(response2)
-            displayWorksModal()
-            displayWorks()
-            closeModal(e)
-          })
-        })
-        .catch(error => {
-          alert("Une erreur est survenue lors de l'ajout de l'image : " + error.message);
-        });
-    });
-
-    document.getElementById("file").addEventListener('change', function (e) {
-      e.preventDefault();
-      const file = e.target.files[0];
-      formData = new FormData();
-    
-    
-      // Vérifier la taille de l'image (maximum de 4 Mo)
-      const maxSize = 4 * 1024 * 1024; // 4 Mo en octets
-      if (file.size > maxSize) {
-        alert("Erreur: la taille de l'image ne doit pas dépasser 4 Mo.");
-        return;
-      }
-    
-      // Vérifier si le fichier est au format PNG, JPEG ou PJPEG
-      const validImageFormats = ["image/png", "image/jpeg", "image/pjpeg"];
-      if (!validImageFormats.includes(file.type)) {
-        alert("Erreur: format de l'image non valide.");
-        return;
-      }
-    
-      formData.append("image", file);
-    
-      const imagePreview = document.getElementById('image-preview');
-      const uploadBtnContainer = document.getElementById('upload-btn-container');
-    
-      const reader = new FileReader();
-      reader.onload = function (event) {
-        imagePreview.src = event.target.result;
-        // Rendre la miniature visible
-        imagePreview.style.display = "block";
-    
-        // Masquer l'icône et le bouton "Ajouter Image"
-        uploadBtnContainer.style.display = "none";
-      };
-    
-      reader.onerror = function () {
-        // Afficher l'icône et le bouton "Ajouter Image" en cas d'erreur
-        imagePreview.style.display = "none";
-        uploadBtnContainer.style.display = "block";
-    
-        alert("Erreur lors du chargement de l'image !");
-      };
-    
-      reader.readAsDataURL(file);
-    });
-    
-    
   }
 }
 
-
+// Fonction pour supprimer projet
 function deleteWork(workToDeletedId) {
   if (confirm("Souhaitez-vous vraiment supprimer ce projet ?") == true) {
     fetch(worksUrl + '/' + workToDeletedId, {
